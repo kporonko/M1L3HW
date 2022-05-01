c206ae3768222214cd9b365fa459167d15b6a317
KeeYoo <poronko2045@gmail.com>
2022-05-01 15:26:43 +0300

    Added user`s array length input with exception catching. The array is created and filled.

--- M1L3HW/M1L3HW/Program.cs
+++ M1L3HW/M1L3HW/Program.cs
@@ -6,7 +6,35 @@ namespace M1L3HW
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Add an array length");
+
+            // Catch an exception while user inputs not a number or number that < 0.
+            int arrLength = -1;
+            while (arrLength < 0)
+            {
+                try
+                {
+                    arrLength = Convert.ToInt32(Console.ReadLine());
+                    if (arrLength < 0)
+                    {
+                        Console.WriteLine("A length must be > 0");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Add a number, not letters");
+                }
+            }
+            
+            //Create an array and fill it by numbers from 1 to 26.
+            int[] startArr = new int[arrLength];
+            for (int i = 0; i < arrLength; i++)
+            {
+                startArr[i] = new Random().Next(1,26);
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
