f550f781c0fdbabb364c29c3367eb3d8634f47fb
KeeYoo <poronko2045@gmail.com>
2022-05-01 15:00:40 +0300

    Console app is added

---
+++ M1L3HW/M1L3HW/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace M1L3HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
